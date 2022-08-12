182ab07d71d067d0cc00109a8496859bf777e5b6
EC2 Default User <ec2-user@ip-172-31-8-34.us-west-2.compute.internal>
2022-08-12 12:28:42 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,35 @@
 class ListsController < ApplicationController
+
   def new
+   @list = List.new
+  end
+
+  def create
+
+    list = List.new(list_params)
+
+    list.save
+
+    redirect_to '/top'
+
   end
 
   def index
+    @list = List.all
   end
 
   def show
   end
 
+
   def edit
+　end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
+  end
+
 end
